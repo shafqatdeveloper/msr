32e2f7ac403631acfe2193185ee439453796ff86
Muhammad Shafqat Rasool <mshafqat.developer@gmamil.com>
2024-01-30 11:42:54 +0500

    Home

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -58,19 +58,19 @@ const Navbar = () => {
         </div>
         <div
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 transition-all duration-500 ease-in ${
-            open ? "top-[46px] backdrop-blur-3xl h-[90vh]" : "top-[-490px]"
+            open ? "top-[46px] backdrop-blur-3xl" : "top-[-490px]"
           }`}
         >
-          <ul className="flex flex-col pb-4 md:flex-row lg:gap-7 mt-10 md:mt-0 md:pb-0">
+          <ul className="flex flex-col pb-4 md:flex-row lg:gap-7 gap-6 mt-10 md:mt-0 md:pb-0">
             {navItems.map((link) => (
               <li
-                className=" dark:hover:bg-white/10 py-2 pl-7 hover:bg-gray-200 w-full"
+                className=" dark:hover:bg-white/10 py-2 pl-7 md:pl-1 md:pr-1 hover:bg-gray-400 w-full"
                 onClick={() => setOpen(!open)}
                 key={`/${link.name}`}
               >
                 <Link
                   href={link.name}
-                  className="w-full capitalize flex items-center gap-3 md:gap-2.5 duration-500"
+                  className="w-full capitalize flex items-center gap-3 md:gap-1.5 duration-500"
                 >
                   <span className="">{link.icon}</span>
                   <span className="tracking-wider md:tracking-wide">
